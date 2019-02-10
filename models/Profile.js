338dca46c22cadc013e65db3d316817a2085c155
Ilyas Keskin <ilyas.keskin@posteo.de>
2019-02-10 16:25:23 +0100

    added profile route, model and validation

---
+++ models/Profile.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  handle: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 40
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "diverse"]
+  },
+  country: {
+    type: String
+  },
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zip: Number
+  },
+  club: {
+    type: String
+  },
+  social: {
+    telephone: String,
+    website: String,
+    facebook: String,
+    youtube: String,
+    twitter: String,
+    instagram: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
---
+++ validation/profile.js
@@ -0,0 +1,69 @@
+const Joi = require("joi");
+const customJoi = Joi.extend(require("joi-phone-number"));
+
+const address = customJoi.object().keys({
+  street: customJoi
+    .string()
+    .min(2)
+    .max(40),
+  city: customJoi
+    .string()
+    .min(2)
+    .max(50),
+  state: customJoi
+    .string()
+    .min(2)
+    .max(30),
+  zip: customJoi.number()
+});
+
+const social = customJoi.object().keys({
+  telephone: customJoi.string().phoneNumber(),
+  website: customJoi.string().uri(),
+  facebook: customJoi.string().uri(),
+  youtube: customJoi.string().uri(),
+  twitter: customJoi.string().uri(),
+  instagram: customJoi.string().uri()
+});
+
+const schema = customJoi.object().keys({
+  handle: customJoi
+    .string()
+    .min(2)
+    .max(40)
+    .default(""),
+  gender: customJoi.string().valid("male", "female", "diverse"),
+  country: customJoi
+    .string()
+    .min(2)
+    .max(40),
+  address,
+  club: customJoi
+    .string()
+    .min(2)
+    .max(40)
+    .default(""),
+  social
+});
+
+const onProfileCreationSchema = schema.requiredKeys(
+  "handle",
+  "address.street",
+  "address.city",
+  "address.state",
+  "address.zip"
+);
+
+module.exports = function validateProfileInput(data, method) {
+  let result;
+  if (method === "POST") {
+    result = customJoi.validate(data, onProfileCreationSchema, {
+      abortEarly: false
+    });
+  } else if (method === "PUT") {
+    result = customJoi.validate(data, schema, {
+      abortEarly: false
+    });
+  }
+  return result;
+};
