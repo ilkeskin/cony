b4c8e9b9e332affed49e72596118ba8b7b8c3890
Ilyas Keskin <ilyas.keskin@posteo.de>
2019-08-18 21:33:14 +0200

    reworked profile route to use async/await pattern

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -11,126 +11,126 @@ const validateProfileInput = require("../../validation/profile");
 
 // @route   GET api/profiles
 // @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate("user", "name");
+        if (profiles.length === 0) {
+            return res.status(404).json({ message: "There are no profiles" });
+        }
+        res.json(profiles);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server error");
+    }
+});
+
+// @route   GET api/profiles/me
+// @desc    Get my profile
 // @access  Private
-router.get("/", auth, (req, res) => {
-    Profile.find()
-        .populate("user", "name")
-        .then(profiles => {
-            if (!profiles) {
-                return res.status(404).json({ message: "There are no profiles" });
-            }
-            res.json(profiles);
-        })
-        .catch(err => res.status(500).json(err));
+router.get("/me", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate("user", "name");
+        if (!profile) {
+            return res.status(404).json({ message: "There is no profile for the current user" });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server error");
+    }
 });
 
 // @route   GET api/profiles/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
-router.get("/handle/:handle", (req, res) => {
-    Profile.findOne({ handle: req.params.handle })
-        .populate("user", "name")
-        .then(profile => {
-            if (!profile) {
-                res.status(404).json({ message: "There is no profile for the user with the given handle" });
-            }
-            res.json(profile);
-        })
-        .catch(err => res.status(500).json(err));
+router.get("/handle/:handle", async (req, res) => {
+    try {
+        let profile = await Profile.findOne({ handle: req.params.handle }).populate("user", "name");
+        if (!profile) {
+            return res.status(404).json({ message: "There is no profile for the user with the given handle" });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server error");
+    }
 });
 
 // @route   GET api/profiles/user/:user_id
 // @desc    Get profile by user ID
 // @access  Public
-router.get("/user/:user_id", (req, res) => {
-    Profile.findOne({ user: req.params.user_id })
-        .populate("user", "name")
-        .then(profile => {
-            if (!profile) {
-                res.status(404).json({ message: "There is no profile for the user with the given ID" });
-            }
-            res.json(profile);
-        })
-        .catch(err => res.status(500).json(err));
+router.get("/user/:user_id", async (req, res) => {
+    try {
+        let profile = await Profile.findOne({ user: req.params.user_id }).populate("user", "name");
+        if (!profile) {
+            res.status(404).json({ message: "Profile not found" });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        if (err.kind == "ObjectId") {
+            return res.status(404).json({ message: "Profile not found" });
+        }
+        res.status(500).send("Server error");
+    }
 });
 
 // @route   POST api/profiles
 // @desc    Create profile for current user
 // @access  Private
-router.post("/", auth, (req, res) => {
+router.post("/", auth, async (req, res) => {
     const { error, value } = validateProfileInput(req.body, req.method);
-
-    // Check validation result
     if (error !== null) {
         return res.status(400).json(error.details);
     }
 
-    // Parse fields from body
-    const profileFields = parseProfileFromBody(req);
-    Profile.findOne({ user: req.user.id }).then(profile => {
+    try {
+        const profileFields = parseProfileFromBody(req);
+
+        let profile = await Profile.findOne({ user: req.user.id });
+
         if (profile) {
-            res.status(409).json({ message: "A profile for this user already exists" });
+            return res.status(409).json({ message: "A profile for this user already exists" });
         } else {
-            // Create
-            // Check if handle exist
-            Profile.findOne({ handle: profileFields.handle }).then(profile => {
-                if (profile) {
-                    return res.status(400).json({ message: "That handle already exists" });
-                }
-            });
-
-            // Save the profile
-            new Profile(profileFields).save().then(profile => res.json(profile));
+            let profileHandleExist = await Profile.findOne({ handle: profileFields.handle });
+            if (profileHandleExist) {
+                return res.status(400).json({ message: "That handle already exists" });
+            }
         }
-    });
+
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server error");
+    }
 });
-
-// @route   PUT api/profiles/user/:user_id
-// @desc    Modify profile for current user
-// @access  Private
-// router.put("/user/:user_id", auth, (req, res) => {
-//     if (req.user.id === req.params.user_id) {
-//         const { error, value } = validateProfileInput(req.body, req.method);
-
-//         // Check validation result
-//         if (error !== null) {
-//             return res.status(400).json(error.details);
-//         }
-
-//         const profileFields = parseProfileFromBody(req);
-//         Profile.findOneAndUpdate(req.params.user_id, profileFields, { new: true })
-//             .then(profile => {
-//                 if (profile) {
-//                     res.json(profile);
-//                 } else {
-//                     res.status(404).json({ message: "The profile for this user does not exist" });
-//                 }
-//             });
-//     } else {
-//         res.status(403).json({ message: "Cannot update the profile of another user" });
-//     }
-// });
 
 // @route   PUT api/profiles
 // @desc    Modify profile for current user
 // @access  Private
-router.put("/", auth, (req, res) => {
+router.put("/", auth, async (req, res) => {
     const { error, value } = validateProfileInput(req.body, req.method);
-
-    // Check validation result
     if (error !== null) {
         return res.status(400).json(error.details);
     }
 
-    const profileFields = parseProfileFromBody(req);
-    Profile.findOneAndUpdate(req.user.id, profileFields, { new: true })
-        .then(profile => {
-            if (profile) {
-                res.json(profile);
-            } else {
-                res.status(404).json({ message: "The profile for this user does not exist" });
-            }
-        });
+    try {
+        const profileFields = parseProfileFromBody(req);
+
+        let profile = await Profile.findOneAndUpdate({ user: req.user.id }, profileFields, { new: true });
+        console.log(profile);
+        if (profile) {
+            res.json(profile);
+        } else {
+            res.status(404).json({ message: "The profile for this user does not exist" });
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server error");
+    }
 });
 
 
