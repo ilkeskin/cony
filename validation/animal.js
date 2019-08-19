0874ca134cc55c03e334e825f15189f180bca21c
Ilyas Keskin <ilyas.keskin@posteo.de>
2019-08-19 21:36:30 +0200

    added mother and father objectid validation for animal

--- validation/animal.js
+++ validation/animal.js
@@ -1,12 +1,13 @@
 const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi);
 
 const tattoo = Joi.object().keys({
     // ^ asserts position at start of a line
-    // Match a single character present in the list below [A-Z]{1,2}
+    // Match a single character present in the list [A-Z]{1,2}
     // {1,2} Quantifier — Matches between 1 and 2 times, as many times as possible, giving back as needed
-    // Match a single character present in the list below [J]?
+    // Match a single character present in the list [J]?
     // ? Quantifier — Matches between zero and one times, as many times as possible, giving back as needed
-    // Match a single character present in the list below [0-9]{1,3}
+    // Match a single character present in the list [0-9]{1,3}
     // {1,3} Quantifier — Matches between 1 and 3 times, as many times as possible, giving back as needed
     // $ asserts position at the end of a line
     right: Joi.string().trim().regex(/^[A-Z]{1,2}[J]?[0-9]{1,3}$/).required(),
@@ -18,10 +19,11 @@ const schema = Joi.object().keys({
     sex: Joi.string().trim().valid("0,1", "1,0"),
     race: Joi.string().trim().min(3).max(50),
     color: Joi.string().trim().min(3).max(50),
-    dateOfBirth: Joi.date().max(Joi.ref("dateOfDeath")),
+    dateOfBirth: Joi.date(),
     dateOfDeath: Joi.date().min(Joi.ref("dateOfBirth")),
-    dateOfSlaughter: Joi.date().min(Joi.ref("dateOfDeath")),
     tattoo,
+    mother: Joi.objectId().max(100),
+    father: Joi.objectId().max(100),
 });
 
 const onAnimalCreationSchema = schema.requiredKeys("name", "sex");
